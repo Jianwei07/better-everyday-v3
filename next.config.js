cc118d40482c02efcd1ec90ca0f6c1de2a19405e
Jayden Liaw <liawjianwei@outlook.com>
2025-05-14 19:52:45 +0800

    Changes for fixes

--- next.config.js
+++ next.config.js
@@ -1,30 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
+    // Default values if environment variables are missing
+    const devUrl = process.env.FASTAPI_DEV_URL || "http://127.0.0.1:8000";
+    const prodUrl =
+      process.env.FASTAPI_PROD_URL || "https://better-everyday-v3.vercel.app";
+
     return [
       // Rewrites for API calls to FastAPI during development vs. production
       {
         source: "/api/py/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? `${process.env.FASTAPI_DEV_URL}/:path*` // Use environment variable for local FastAPI
-            : `${process.env.FASTAPI_PROD_URL}/api/py/:path*`, // Use environment variable for production FastAPI
+            ? `${devUrl}/:path*`
+            : `${prodUrl}/api/py/:path*`,
       },
       // Rewrites for API documentation in FastAPI
       {
         source: "/docs",
         destination:
           process.env.NODE_ENV === "development"
-            ? `${process.env.FASTAPI_DEV_URL}/docs` // FastAPI docs locally
-            : `${process.env.FASTAPI_PROD_URL}/api/py/docs`, // Production FastAPI docs
+            ? `${devUrl}/docs`
+            : `${prodUrl}/api/py/docs`,
       },
       // Rewrites for OpenAPI JSON schema in FastAPI
       {
         source: "/openapi.json",
         destination:
           process.env.NODE_ENV === "development"
-            ? `${process.env.FASTAPI_DEV_URL}/openapi.json` // Local OpenAPI schema
-            : `${process.env.FASTAPI_PROD_URL}/api/py/openapi.json`, // Production OpenAPI schema
+            ? `${devUrl}/openapi.json`
+            : `${prodUrl}/api/py/openapi.json`,
       },
     ];
   },
